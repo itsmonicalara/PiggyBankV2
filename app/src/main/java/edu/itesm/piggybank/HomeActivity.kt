9a3dd60e55e79c574dd7bffbdc9123f90e3291ea
Monica Lara <A01655306@itesm.mx>
2021-03-15 00:20:28 -0600

    Splash screen ready

---
+++ app/src/main/java/edu/itesm/piggybank/HomeActivity.kt
@@ -0,0 +1,23 @@
+package edu.itesm.piggybank
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed({
+            val intento = Intent(this, MainActivity::class.java)
+            startActivity(intento)
+            finish()
+        }, 7000)
+    }
+}
