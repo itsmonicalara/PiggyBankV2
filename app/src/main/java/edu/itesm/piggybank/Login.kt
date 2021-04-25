f1be76c07558879041626d82c985c41d35fc9a06
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2021-04-24 20:31:40 -0500

    Minor changes

--- app/src/main/java/edu/itesm/piggybank/Login.kt
+++ app/src/main/java/edu/itesm/piggybank/Login.kt
@@ -46,6 +46,7 @@ class Login : Fragment() {
             ).addOnCompleteListener{
                 if(it.isSuccessful){
                     Toast.makeText(this.context,"Bienvenido", Toast.LENGTH_LONG).show()
+
                     view?.findNavController()?.navigate(R.id.loginToFirst)
                 }else{
                     Toast.makeText(this.context,"Usuario o contraseña son incorrectos", Toast.LENGTH_LONG).show()
