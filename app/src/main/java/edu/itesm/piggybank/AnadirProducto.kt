e4671c813bc2469dbf07e3943a90b67f6705d333
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2021-05-25 00:06:39 -0500

    Template done

--- app/src/main/java/edu/itesm/piggybank/AnadirProducto.kt
+++ app/src/main/java/edu/itesm/piggybank/AnadirProducto.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import edu.itesm.piggybank.databinding.FragmentAnadirProductoBinding
 
 
 class AnadirProducto : Fragment() {
 
+    private var binding: FragmentAnadirProductoBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +22,16 @@ class AnadirProducto : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anadir_producto, container, false)
+        binding = FragmentAnadirProductoBinding.inflate(inflater, container, false)
+        val categories = resources.getStringArray(R.array.categoria)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_item, categories)
+        binding!!.autoCompleteTextView3.setAdapter(arrayAdapter)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
 
