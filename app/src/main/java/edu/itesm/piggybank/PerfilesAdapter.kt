d4314ddab5556c6146e8ab432900eac67efff2fd
caposcar1998 <oscarcontrerascap@gmail.com>
2021-06-09 19:26:46 -0500

    se añade saldo

--- app/src/main/java/edu/itesm/piggybank/PerfilesAdapter.kt
+++ app/src/main/java/edu/itesm/piggybank/PerfilesAdapter.kt
@@ -1,36 +1,47 @@
 package edu.itesm.piggybank
 
+
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
 class PerfilesAdapter(private val perfiles : List<Perfil>)
     : RecyclerView.Adapter<PerfilesAdapter.PerfilViewHolder>(){
 
+    lateinit var tipoGasto: String
+    var cantidad = 0
+
     inner class PerfilViewHolder(renglon: View) : RecyclerView.ViewHolder(renglon){
+
         var nombre = renglon.findViewById<TextView>(R.id.nombre)
         var foto = renglon.findViewById<ImageView>(R.id.foto)
     }
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PerfilViewHolder {
+
         val inflater = LayoutInflater.from(parent.context)
         val renglon_vista = inflater.inflate(R.layout.perfiles_renglon,parent, false)
         return PerfilViewHolder(renglon_vista)
     }
 
     override fun onBindViewHolder(holder: PerfilViewHolder, position: Int) {
+
         val perfil = perfiles[position]
         holder.foto.setImageResource(perfil.picture)
         holder.nombre.text = perfil.tipo
         holder.itemView.setOnClickListener(){
-            val action = PerfilesFragmentDirections.actionPerfilesFragmentToPerfilFragment(perfil)
-            holder.itemView?.findNavController()?.navigate(action)
+            tipoGasto = perfil.tipo
+            Log.e("error",tipoGasto)
         }
     }
+
+
 
     override fun getItemCount(): Int {
         return perfiles.size
--- app/src/main/java/edu/itesm/piggybank/PerfilesFragment.kt
+++ app/src/main/java/edu/itesm/piggybank/PerfilesFragment.kt
@@ -1,26 +1,38 @@
 package edu.itesm.piggybank
 
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.fragment_editar_perfil.*
 import kotlinx.android.synthetic.main.fragment_perfiles.*
+import java.io.ByteArrayOutputStream
+import java.util.*
+import kotlin.collections.ArrayList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PerfilesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PerfilesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
+    private lateinit var dataBase : FirebaseFirestore
+    private var clave = ""
+    private  var cochinito = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dataBase = Firebase.firestore
+        val reference = dataBase.collection("users")
+        getDataUser()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +48,9 @@ class PerfilesFragment : Fragment() {
             layoutManager = LinearLayoutManager(activity)
             adapter = PerfilesAdapter(createData())
         }
+        actualizarSaldo.setOnClickListener{
+            nuevaConfiguracion()
+        }
     }
 
     fun createData(): ArrayList<Perfil>{
@@ -48,6 +63,69 @@ class PerfilesFragment : Fragment() {
         perfiles.add(Perfil(R.drawable.domingo, "Domingo"))
         perfiles.add(Perfil(R.drawable.cumpleanos, "Cumpleaños"))
         return perfiles
+    }
+
+    fun getDataUser(){
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val email = user.email
+        }
+        val capitalCities = dataBase.collection("users")
+        capitalCities.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    if(document.data.get("correo").toString() == user.email.toString()){
+                        Log.d(ContentValues.TAG, "existe")
+                        clave = document.data.get("correo").toString()
+                        cochinito = document.data.get("cochinito").toString().toDouble()
+
+
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error getting documents: ", exception)
+            }
+
+    }
+
+    fun nuevaConfiguracion(){
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val email = user.email
+        }
+        if(editTextNumber != null){
+
+
+                    val capitalCities = dataBase.collection("users")
+                    capitalCities.get()
+                        .addOnSuccessListener { documents ->
+                            for (documentGot in documents) {
+                                if(documentGot.data.get("correo").toString() == user.email.toString()){
+                                    val data = hashMapOf("incrementos" to editTextNumber.text.toString())
+                                    Log.e("base",cochinito.toString())
+                                    Log.e("local",editTextNumber.text.toString())
+                                    var data2 =hashMapOf("cochinito" to (  editTextNumber.text.toString().toDouble() + cochinito))
+                                    dataBase.collection("users").document(documentGot.id.toString())
+                                        .set(
+                                            data,SetOptions.merge()
+                                        )
+                                    dataBase.collection("users").document(documentGot.id.toString())
+                                        .set(
+                                            data2,SetOptions.merge()
+                                        )
+                                }
+                            }
+                        } .addOnFailureListener { exception ->
+                            Log.w(ContentValues.TAG, "Error getting documents: ", exception)
+                        }
+
+
+        }else{
+            Toast.makeText(this.context,"Tienes que seleccionar una foto para poder actualizar", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 
 }
